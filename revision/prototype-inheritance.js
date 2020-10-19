6be4871891d8ac2a11983d500b5b101403683978
aman <aman.khan.silawat.aks@gmail.com>
2020-10-19 18:43:52 +0530

    Example inheritance prototype

---
+++ revision/prototype-inheritance.js
@@ -0,0 +1,34 @@
+// create Person function
+function Person(fName, lName, producName) {
+    this.fName = fName;
+    this.lName = lName;
+    this.producName = producName;
+}
+
+// create a method
+Person.prototype.getFullName = function () {
+    return `${this.fName} ${this.lName}`;
+};
+
+//create constructor functing using new keyword
+person1 = new Person('Aman', 'Silawat', 'Dell i3 Laptop');
+person1.getFullName(); // Aman Silawat
+
+
+// Create Another function
+function StudentPerson(fName, lName, producName, schoolId) {
+    let paidUser = Person.call(this, fName, lName, producName);
+    this.schoolId = schoolId;
+}
+
+//inherite prototype
+StudentPerson.prototype =  Object.create(Person.prototype);
+
+StudentPerson.prototype.getId = function() {
+    return this.schoolId;
+}
+
+//create constructor functing using new keyword
+student = new StudentPerson('Aman', 'Silawat', 'Dell i3 Laptop', '10255');
+student.getId(); // 10255
+student.getFullName(); // Aman Silawat
